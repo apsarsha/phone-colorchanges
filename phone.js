e3dab4360b1aea71b1b4ca3ef5d334e0178487b7
shaik apsar <apsarsk823@gmail.com>
2022-08-18 19:47:39 +0530

    phone-color

---
+++ phone.js
@@ -0,0 +1,48 @@
+const phoneImages = ["images/android.jpg ", "images/iphone.jpg  ", "images/oppo.jpg  ","images/poko.jpg ", "images/redmi.jpg","images/vivo.jpg" ]
+let phoneCont = document.querySelector(".phone-image")
+let black = document.querySelector(".android");
+
+let blue = document.querySelector(".iphone")
+
+let pink = document.querySelector(".oppo")
+
+let starlight= document.querySelector(".poko")
+let moonlight= document.querySelector(".redmi")
+let otherlight= document.querySelector(".vivo")
+
+
+let defaultImgItems =`<img src= "${phoneImages.at(0)}">`
+
+phoneCont.innerHTML = defaultImgItems
+
+let blueImgItems = `<img  src= "${phoneImages.at(1)}">`
+let pinkImgItems = `<img "w-100" src= "${phoneImages.at(2)}">`
+let starImgItems = `<img "w-100" src= "${phoneImages.at(3)}">`
+let moonlightImgItems = `<img "w-100" src= "${phoneImages.at(4)}">`
+
+let otherlightImgItems = `<img "w-100" src= "${phoneImages.at(5)}">`
+
+black.addEventListener("click", function(){
+    phoneCont.innerHTML=defaultImgItems
+    })
+blue.addEventListener("click", function () {
+    phoneCont.innerHTML = blueImgItems
+})
+
+pink.addEventListener("click", function () {
+    phoneCont.innerHTML = pinkImgItems
+})
+
+starlight.addEventListener("click", function () {
+    phoneCont.innerHTML = starImgItems
+})   
+
+
+moonlight.addEventListener("click", function () {
+    phoneCont.innerHTML = moonlightImgItems
+})   
+
+
+otherlight.addEventListener("click", function () {
+    phoneCont.innerHTML = otherlightImgItems
+})   
